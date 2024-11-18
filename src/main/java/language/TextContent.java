cce84565f4700f414202372fd87f16611360a4ce
DigammaF <digamma.for@gmail.com>
2024-11-18 22:52:51 +0100

    Added text content for the restaurant editor

--- src/main/java/language/TextContent.java
+++ src/main/java/language/TextContent.java
@@ -10,6 +10,13 @@ public class TextContent {
         MENU_TICKETS_EDITOR_BUTTON,
         MENU_RESTAURANT_EDITOR_BUTTON,
         PRODUCT_DISPLAY_NAME_LABEL,
+        RESTAURANT_EDITOR_NAME_LABEL,
+        RESTAURANT_EDITOR_ADDRESS_LABEL,
+        RESTAURANT_EDITOR_PHONE_LABEL,
+        RESTAURANT_EDITOR_EMAIL_LABEL,
+        RESTAURANT_EDITOR_TAXID_LABEL,
+        RESTAURANT_EDITOR_SIREN_LABEL,
+        RESTAURANT_EDITOR_LATE_PENALTY_POLICY_LABEL,
     }
 
     public enum Language {
@@ -29,6 +36,13 @@ public class TextContent {
         this.texts.put(Key.MENU_TICKETS_EDITOR_BUTTON, make("Ticket editor", "Editeur de ticket"));
         this.texts.put(Key.MENU_RESTAURANT_EDITOR_BUTTON, make("Restaurant editor", "Editeur de restaurant"));
         this.texts.put(Key.PRODUCT_DISPLAY_NAME_LABEL, make("Name", "Nom"));
+        this.texts.put(Key.RESTAURANT_EDITOR_NAME_LABEL, make("Name", "Nom"));
+        this.texts.put(Key.RESTAURANT_EDITOR_ADDRESS_LABEL, make("Address", "Adresse"));
+        this.texts.put(Key.RESTAURANT_EDITOR_PHONE_LABEL, make("Phone", "Téléphone"));
+        this.texts.put(Key.RESTAURANT_EDITOR_EMAIL_LABEL, make("Email", "Email"));
+        this.texts.put(Key.RESTAURANT_EDITOR_TAXID_LABEL, make("Tax ID", "Numéro de TVA"));
+        this.texts.put(Key.RESTAURANT_EDITOR_SIREN_LABEL, make("SIREN", "SIREN"));
+        this.texts.put(Key.RESTAURANT_EDITOR_LATE_PENALTY_POLICY_LABEL, make("Late penalty policy", "Politique de pénalité de retard"));
     }
 
     static private Map<Language, String> make(String en, String fr) {
