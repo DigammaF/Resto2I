40d8cf9e142444c81fcc584a3ad44fb323f0a107
mchanteg <maxime.chantegrel@ig2i.com>
2024-12-02 09:06:30 +0100

    Added demo mode selection from command-line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 import views.AppContext;
 import views.MainView;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        AppContext.getAppContext(true);
+        AppContext.getAppContext(Objects.equals(args[0], "demoMode"));
         MainView view = new MainView();
         view.setVisible(true);
     }
