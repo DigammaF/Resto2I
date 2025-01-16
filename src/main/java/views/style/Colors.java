ec9eb48ea8641164d410ca247be062090fd25ad2
DigammaF <digamma.for@gmail.com>
2025-01-16 22:11:33 +0100

    Changed field coloring

--- src/main/java/views/style/Colors.java
+++ src/main/java/views/style/Colors.java
@@ -3,7 +3,7 @@ package views.style;
 import java.awt.*;
 
 public class Colors {
-    public static Color MODIFIED_FIELD = Color.red;
+    public static Color MODIFIED_FIELD = Color.yellow;
     public static Color COMMITED_FIELD = Color.green;
     public static Color UNMODIFIED_FIELD = Color.white;
     public static Color STRANGE_VALUE_FIELD = Color.orange;
